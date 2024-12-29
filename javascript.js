ae7f650180795dfe23930950730f24d4693ec70e
Joseph Peter Kamvabingu <josephpeterkamvabingu@gmail.com>
2024-12-29 06:55:35 +0200

    Force push all files

---
+++ javascript.js
@@ -0,0 +1,14 @@
+@media (max-width: 768px) {
+    #scrollToTop {
+      width: 40px;
+      height: 40px;
+      line-height: 40px;
+      bottom: 15px;
+      right: 15px;
+    }
+  
+    #scrollToTop i {
+      font-size: 20px;
+    }
+  }
+  
